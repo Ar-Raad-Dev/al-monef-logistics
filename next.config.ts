f493cbd107d9ff00f60d4e21ae53c6e1abcd017f
Khursheed Ahmad <khursheed.ahmad.ksa1@gmail.com>
2025-06-14 05:32:54 +0000

    THIS ABOUT PAGE IMAGE NOT LOADING

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,14 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* Next.js configuration options */
   images: {
-    // No remote patterns needed if all images are local
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'placehold.co',
-    //     port: '',
-    //     pathname: '/**',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
